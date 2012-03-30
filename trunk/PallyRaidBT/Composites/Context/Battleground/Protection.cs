9ff661a9f3f4f33d5087a481d9b98c29e1249766
camhaider@gmail.com <camhaider@gmail.com@2f3fbc23-2c7c-44c4-db58-f9f15bdb9f4d>
2012-03-30 05:11:30 +0000

    upload v0.1

---
+++ trunk/PallyRaidBT/Composites/Context/Battleground/Protection.cs
@@ -0,0 +1,29 @@
+﻿//////////////////////////////////////////////////
+//           Battleground/Protection .cs        //
+//        Part of PallyRaidBT by kaihaider      //
+//////////////////////////////////////////////////
+//   Originally from MutaRaidBT by fiftypence.  //
+//    Reused with permission from the author.   //
+//////////////////////////////////////////////////
+
+using TreeSharp;
+namespace PallyRaidBT.Composites.Context.Battleground
+{
+    class Protection
+    {
+        static public Composite BuildCombatBehavior()
+        {
+            return Raid.Protection.BuildCombatBehavior();
+        }
+
+        static public Composite BuildPullBehavior()
+        {
+            return Raid.Protection.BuildPullBehavior();
+        }
+
+        static public Composite BuildBuffBehavior()
+        {
+            return Raid.Protection.BuildBuffBehavior();
+        }
+    }
+}
---
+++ trunk/PallyRaidBT/Composites/Context/Level/None.cs
@@ -0,0 +1,31 @@
+﻿//////////////////////////////////////////////////
+//                   Leve/None.cs               //
+//        Part of PallyRaidBT by kaihaider      //
+//////////////////////////////////////////////////
+//   Originally from MutaRaidBT by fiftypence.  //
+//    Reused with permission from the author.   //
+//////////////////////////////////////////////////
+
+using Styx;
+using TreeSharp;
+
+namespace PallyRaidBT.Composites.Context.Level
+{
+    class None
+    {
+        static public Composite BuildCombatBehavior()
+        {
+            return Raid.Retribution.BuildCombatBehavior();
+        }
+
+        static public Composite BuildPullBehavior()
+        {
+            return Raid.Retribution.BuildPullBehavior();
+        }
+
+        static public Composite BuildBuffBehavior()
+        {
+            return Raid.Retribution.BuildBuffBehavior();
+        }
+    }
+}
