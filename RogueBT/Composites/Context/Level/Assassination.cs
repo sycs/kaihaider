d87f5b0baad44241fa8d9c5c5bd4d3a9fd228b9e
camhaider <camhaider@gmail.com>
2013-01-10 07:10:28 +0000

    disabled disarm...

--- RogueBT/Composites/Context/Level/Assassination.cs
+++ RogueBT/Composites/Context/Level/Assassination.cs
@@ -52,7 +52,7 @@ namespace RogueBT.Composites.Context.Level
                             !Helpers.Aura.IsTargetImmuneStun && Helpers.Movement.IsInSafeMeleeRange),
                         Helpers.Spells.Cast("Combat Readiness", ret => Helpers.Target.mNearbyEnemyUnits.Count(unit => unit.Distance <= 10) > 1),
                         Helpers.Spells.CastSelf("Cloak of Shadows", ret => Helpers.Rogue.IsCloakUsable()),
-                        Helpers.Spells.Cast("Dismantle", ret => Helpers.Rogue.mTarget.IsHumanoid),
+                        //Helpers.Spells.Cast("Dismantle", ret => Helpers.Rogue.mTarget.IsHumanoid),
                         Helpers.Spells.Cast("Shiv", ret => Helpers.Aura.Leeching && Helpers.Rogue.mHP < 25)
                     )
                 ),
--- RogueBT/Composites/Context/Level/Subtlety.cs
+++ RogueBT/Composites/Context/Level/Subtlety.cs
@@ -61,7 +61,7 @@ namespace RogueBT.Composites.Context.Level
                         Helpers.Spells.CastSelf("Evasion", ret => Helpers.Rogue.mTarget != null && !Helpers.Rogue.mTarget.Stunned),
                         Helpers.Spells.Cast("Combat Readiness", ret => Helpers.Target.mNearbyEnemyUnits.Count(unit => unit.Distance <= 10) > 1),
                         Helpers.Spells.CastSelf("Cloak of Shadows", ret => Helpers.Rogue.IsCloakUsable()),
-                        Helpers.Spells.Cast("Dismantle", ret => Helpers.Rogue.mTarget.IsHumanoid),
+                        //Helpers.Spells.Cast("Dismantle", ret => Helpers.Rogue.mTarget.IsHumanoid),
                         Helpers.Spells.Cast("Shiv", ret => Helpers.Aura.Leeching && Helpers.Rogue.mHP < 25)
                     )
                 ),
